e4fe02fdd5ef0cc1f3fa8889d73a42e419658ab3
naridev <narishin.ns@gmail.com>
2019-05-05 23:09:20 -0700

    challenge 5 without modular

---
+++ challenge.js
@@ -0,0 +1,61 @@
+var ship = {
+    powerOn: false,
+    modules: [],
+    antenna: {
+      active: false
+    }
+  };
+  
+  const availableModules = [
+    { name: "life-support", size: 10, enabled : false, essential: true },
+    { name: "Module 2",     size: 20, enabled : true , essential: false},
+    { name: "propulsion",   size: 30, enabled : false, essential: true },
+    { name: "Module 4",     size: 40, enabled : true , essential: false},
+    { name: "Module 5",     size: 50, enabled : false, essential: true },
+  ];
+
+function powerOn() {
+  ship.powerOn= true;
+}
+
+function countModules() {
+  return availableModules.length;
+}
+
+function countEssential() {
+  var count = 0;
+  for (var i =0; i< availableModules.length; i++){
+    if (availableModules[i].essential === true){
+      count++;
+    }
+  }
+  return count;
+}
+
+function loadModule(index) {
+  availableModules[index].enabled = true;
+  ship.modules.push(availableModules[index]);
+}
+
+function loadSupport() {
+  for (var i =0; i< availableModules.length; i++){
+    if (availableModules[i].name === "life-support"){
+      loadModule(i);
+    }
+  }
+}
+
+loadSupport();
+
+function findModuleIndex(given){
+  for (var i =0; i< availableModules.length; i++){
+    if (availableModules[i].name === given && availableModules[i].essential){
+        return i;
+    }
+  }
+}
+
+console.log("Essential: " + countEssential());
+loadModule(findModuleIndex("propulsion"));
+console.log(availableModules);
+console.log(ship);
